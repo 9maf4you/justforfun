42f3a995a1c1e693d7bb7eda6d0a12df50a57261
themaf <themaf@yandex-team.ru>
2017-04-20 12:22:40 +0300

    some fixes

--- database.py
+++ database.py
@@ -1,16 +1,15 @@
 import sqlite3
-
+dbname = '/tmp/test.db'
 def do_select(who):
-    db = sqlite3.connect('/tmp/test.db')
-    query = "select * from rambler where login = \'{0}\';".format(who)
+    db = sqlite3.connect(dbname)
+    query = "select * from company where login = \'{0}\';".format(who)
     cur = db.execute(query)
     return cur
 
 
-#insert into rambler (login,first_name,last_name) values ("seege","sergey","sevostyanov");
 def do_insert(user):
-    db = sqlite3.connect('/tmp/test.db')
-    query = "insert into rambler (login,first_name,last_name) values (\"{0}\", \"{1}\", \"{2}\");".format(user['login'],
+    db = sqlite3.connect(dbname)
+    query = "insert into company (login,first_name,last_name) values (\"{0}\", \"{1}\", \"{2}\");".format(user['login'],
                                                                                                     user['name'],
                                                                                                     user['last_name'])
     db.execute(query)
